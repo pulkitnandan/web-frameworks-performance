be019bac349ad8c80d0d6a646849078a5d420f16
Rohit Nandan <pulkitnandan@gmail.com>
2017-04-06 17:54:29 +0530

    Fix bug

--- reactjs/expressHtml9MB/server.js
+++ reactjs/expressHtml9MB/server.js
@@ -15,6 +15,6 @@ app.prepare()
 
   server.listen(3001, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on http://localhost:3001')
   })
 })
